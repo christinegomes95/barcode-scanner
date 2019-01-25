9ed3ed6d612fe5eb61a84f0e737e832b93f88ab7
Christine.gomes95 <christineg@provexam.com>
2019-01-25 10:25:00 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MODULE_ROUTES, MODULE_COMPONENT } from './app.routing';
+import { SideCartComponent } from './common/side-cart/side-cart.component';
+import { HeaderComponent } from './common/header/header.component';
+import { AppService } from './app.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideCartComponent,
+    HeaderComponent,
+    MODULE_COMPONENT,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(MODULE_ROUTES)
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
